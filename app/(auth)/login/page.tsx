90306d055380cc69a2f4a6be07ecc123ca654a9f
Joao Almeida <joao-almeida11@outlook.com>
2025-12-14 20:57:47 +0000

    feat: login card

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,31 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="flex min-h-dvh items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-col gap-y-5 border border-slate-100 rounded-lg p-6 shadow-lg bg-white">
+        <header>
+          <h1 className="text-center text-2xl mb-6">Login to your account</h1>
+          <p>Enter your email below to login to your account:</p>
+        </header>
+        <div className="flex flex-col gap-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" placeholder="a@example.com" id="email" />
+        </div>
+        <div className="flex flex-col gap-y-2">
+          <Label htmlFor="password">Password</Label>
+          <Input type="password" id="password" />
+        </div>
+        <Button type="submit">Login</Button>
+        <div className="text-center">
+          Don&apos;t have an account?
+          <Button variant="link" size="sm">
+            Sign up
+          </Button>
+        </div>
+      </main>
+    </div>
+  );
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -32,21 +32,23 @@ const buttonVariants = cva(
     defaultVariants: {
       variant: "default",
       size: "default",
+      type: "button",
     },
-  }
-)
+  },
+);
 
 function Button({
   className,
   variant = "default",
   size = "default",
   asChild = false,
+  type = "button",
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
+    asChild?: boolean;
   }) {
-  const Comp = asChild ? Slot : "button"
+  const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
@@ -54,9 +56,10 @@ function Button({
       data-variant={variant}
       data-size={size}
       className={cn(buttonVariants({ variant, size, className }))}
+      type={type}
       {...props}
     />
-  )
+  );
 }
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
