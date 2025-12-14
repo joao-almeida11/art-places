13f0a3857a444908bd52323b41cdb2b63572fc1b
Joao Almeida <joao-almeida11@outlook.com>
2025-12-14 17:45:52 +0000

    feat: button storybook

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -1,25 +1,25 @@
-import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 
-import { fn } from 'storybook/test';
+import { fn } from "storybook/test";
 
-import { Button } from './Button';
+import { Button } from "@/components/ui/button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: "Atoms/Button",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  // argTypes: {
+  //   backgroundColor: { control: "color" },
+  // },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
-  args: { onClick: fn() },
+  args: { onClick: fn(), children: "Button" },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -27,28 +27,47 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
+  args: {},
+};
+
+export const Destructive: Story = {
   args: {
-    primary: true,
-    label: "Button",
+    variant: "destructive",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    variant: "outline",
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    variant: "secondary",
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: "ghost",
+  },
+};
+
+export const Link: Story = {
+  args: {
+    variant: "link",
   },
 };
 
 export const Large: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
+    size: "lg",
   },
 };
 
 export const Small: Story = {
   args: {
-    size: 'small',
-    label: 'Button',
+    size: "sm",
   },
 };
