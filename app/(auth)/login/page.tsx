02d05b4ffa83c5d6ff21ba6ee01a94bfd4c66382
Joao Almeida <joao-almeida11@outlook.com>
2025-12-15 01:33:09 +0000

    feat: using Form from shadcn in login card

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,40 @@
+"use client";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import {
+  Form,
+  FormField,
+  FormLabel,
+  FormItem,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { emailValidation, passwordValidation } from "@/lib/validations";
+
+const formSchema = z.object({
+  email: emailValidation,
+  password: passwordValidation,
+});
 
 export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <div className="flex min-h-dvh items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-col gap-y-5 border border-slate-100 rounded-lg p-6 shadow-lg bg-white">
@@ -11,23 +42,49 @@ export default function Login() {
           <h1 className="text-center text-3xl mb-6 font-serif">Login</h1>
           <p>Enter your email below to login to your account:</p>
         </header>
-        <div className="flex flex-col gap-y-2">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            type="email"
-            placeholder="Your email: a@example.com"
-            id="email"
-          />
-        </div>
-        <div className="flex flex-col gap-y-2">
-          <Label htmlFor="password">Password</Label>
-          <Input
-            type="password"
-            id="password"
-            placeholder="Please enter your password"
-          />
-        </div>
-        <Button type="submit">Login</Button>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Your email: name@example.com"
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Please enter your password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </form>
+        </Form>
         <div className="text-center">
           Don&apos;t have an account?
           <Link href="/signup">
